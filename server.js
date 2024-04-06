b4d927438725a930f856e2804f978c19e9701b49
もっちゃん <s7056i3056@gmail.com>
2024-04-06 16:56:58 +0900

    feat: prismaでCRUD処理を実装

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const PORT = 8000;
 const prisma = new PrismaClient();
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  return res.json(posts);
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(id),
+    }
+  });
+  return res.json(post);
+});
+
 app.post("/", async (req, res) => {
   const { title, body } = req.body;
   const posts = await prisma.post.create({
@@ -16,6 +31,31 @@ app.post("/", async (req, res) => {
     },
   });
   return res.json(posts);
+});
+
+app.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, body } = req.body;
+  const posts = await prisma.post.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      title: title,
+      body: body,
+    },
+  });
+  return res.json(posts);
+});
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const deletePost = await prisma.post.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.json(deletePost);
 });
 
 app.listen(PORT, () => {
